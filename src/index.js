e08aafdcf78730b245411785005e6feeb22f2b0c
miguelgarcia210 <garcia.a.miguel.210@gmail.com>
2019-11-19 11:59:03 -0600

    Nav Bar menu adjusted + buttons set disabled class

--- src/index.js
+++ src/index.js
@@ -29,9 +29,10 @@ $(document).ready(() => {
     let allMovies = [];
     let selectedTitle = '';
     let selectedRating;
+    let disabledButtons = [];
 
-  // $("main").html("loading...");
-  $("main").html(`<div id="loader-container"><img class="loaderImage" src="img/editLoader.gif"></div>`);
+    // $("main").html("loading...");
+    $("main").html(`<div id="loader-container"><img class="loaderImage" src="img/editLoader.gif"></div>`);
 
     const renderMovieList = (title, rating, id) => {
         let content = `<div class="card">`;
@@ -47,10 +48,10 @@ $(document).ready(() => {
         content += `<div id="collapse${id}" class="collapse" aria-labelledby="heading${id}" data-parent="#myGroup">`;
         content += `<div class="movie-content-container card-body">`;
         content += `<div class="movie-description">Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.</div>`;
-        content += `<button type="button" id="editButton-${id}" class="edit-button btn btn-warning btn-sm" data-toggle="modal" data-target="#modal">
+        content += `<button type="button" id="editButton-${id}" class="edit-button btn btn-warning btn-sm disable-able" data-toggle="modal" data-target="#modal">
                     Edit
                 </button>`;
-        content += `<button type="button" id="deleteButton-${id}" class="delete-button btn btn-danger btn-sm" data-toggle="modal" data-target="#modal">
+        content += `<button type="button" id="deleteButton-${id}" class="delete-button btn btn-danger btn-sm disable-able" data-toggle="modal" data-target="#modal">
                     Delete
                 </button>`;
         content += `</div>`;
@@ -61,31 +62,39 @@ $(document).ready(() => {
     };
 
     const renderList = (title = "Here are all the movies") => {
-        getMovies().then((movieList) => {
-          allMovies = movieList;
-          console.log(allMovies);
-          setTimeout(() => {
-            mainContainer().html(title);
-            mainContainer().append(`<div id="accordion">`);
-            movieList.forEach(({title, rating, id}) => {
-              $("#accordion").append(renderMovieList(title, rating, id));
-              //    placeholder if all else fails
-            });
-            mainContainer().append(`</div>`);
-            function editLabel() {
-                modalLabel = 'Edit Movie';
-                console.log(modalLabel);
-                movieCard = $(this).attr("id").split("-")[1];
-                for (let movie of allMovies) {
-                  if (parseInt(movieCard) === movie.id) {
-                    selectedTitle = movie.title;
-                    selectedRating = movie.rating;
-                  }
-                }
+        getMovies()
+            .then((movieList) => {
+                disabledButtons = $(".disable-able");
+                console.log(disabledButtons);
+                // $("#saveInput").attr("disabled", true);
+                // for (let button of disabledButtons) {
+                //     button.attr("disabled", true);
+                // }
+                allMovies = movieList;
+                console.log(allMovies);
+                setTimeout(() => {
+                    mainContainer().html(title);
+                    mainContainer().append(`<div id="accordion">`);
+                    movieList.forEach(({title, rating, id}) => {
+                        $("#accordion").append(renderMovieList(title, rating, id));
+                        //    placeholder if all else fails
+                    });
+                    mainContainer().append(`</div>`);
 
-                // console.log(parseInt(movieCard));
-                $('#modalLabel').html(modalLabel);
-                $('.modalOne').html(`<form>
+                    function editLabel() {
+                        modalLabel = 'Edit Movie';
+                        console.log(modalLabel);
+                        movieCard = $(this).attr("id").split("-")[1];
+                        for (let movie of allMovies) {
+                            if (parseInt(movieCard) === movie.id) {
+                                selectedTitle = movie.title;
+                                selectedRating = movie.rating;
+                            }
+                        }
+
+                        // console.log(parseInt(movieCard));
+                        $('#modalLabel').html(modalLabel);
+                        $('.modalOne').html(`<form>
                             <div class="form-group">
                                 <label for="titleInput" class="col-form-label">Title:</label>
                                 <input type="text" class="form-control" id="titleInput">
@@ -95,29 +104,37 @@ $(document).ready(() => {
                                 <input type="text" class="form-control" id="ratingInput">
                             </div>
                         </form>`);
-                $('#saveInput').removeClass('btn-danger btn-success').addClass('btn-warning').html('Save Edit')
-                // modalLabel().html('Edit Movie')
-              $("#titleInput").val(selectedTitle);
-              $("#ratingInput").val(selectedRating);
-            }
-            $(".edit-button").click(editLabel);
+                        $('#saveInput').removeClass('btn-danger btn-success').addClass('btn-warning').html('Save Edit')
+                        // modalLabel().html('Edit Movie')
+                        $("#titleInput").val(selectedTitle);
+                        $("#ratingInput").val(selectedRating);
+                    }
 
-            function deleteLabel() {
-                modalLabel = 'Delete Movie';
-                console.log(modalLabel);
-                movieCard = $(this).attr("id").split("-")[1];
-                console.log(movieCard);
-                $('#modalLabel').html(modalLabel);
-                // modalLabel().html('Edit Movie')
-                $('.modalOne').html("Are you sure you want to delete this movie?");
-                $('#saveInput').removeClass('btn-warning btn-success').addClass('btn-danger').html('Delete')
-            }
-            $(".delete-button").click(deleteLabel);
-          },.5);
-        }).catch((error) => {
-            alert('Oh no! Something went wrong.\nCheck the console for details.');
-            console.log(error);
-        });
+                    $(".edit-button").click(editLabel);
+
+                    function deleteLabel() {
+                        modalLabel = 'Delete Movie';
+                        console.log(modalLabel);
+                        movieCard = $(this).attr("id").split("-")[1];
+                        console.log(movieCard);
+                        $('#modalLabel').html(modalLabel);
+                        // modalLabel().html('Edit Movie')
+                        $('.modalOne').html("Are you sure you want to delete this movie?");
+                        $('#saveInput').removeClass('btn-warning btn-success').addClass('btn-danger').html('Delete')
+                    }
+
+                    $(".delete-button").click(deleteLabel);
+                }, .5);
+            })
+            // .then(() => {
+            //     for (let button of disabledButtons) {
+            //         button.removeAttr("disabled", false);
+            //     }
+            // })
+            .catch((error) => {
+                alert('Oh no! Something went wrong.\nCheck the console for details.');
+                console.log(error);
+            });
     };
 
     // getMovies().then((movies) => {
@@ -219,7 +236,7 @@ $(document).ready(() => {
                 .then((renderList(`Movie list updated: ${udate}`)));
         }
     });
-        let username;
+    let username;
 //    Sign in functionality
     $('#signInButton').click(() => {
         if ($('#dropdownFormUsername').val().length < 5) {
